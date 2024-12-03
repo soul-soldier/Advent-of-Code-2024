7db1cdee6676bfa8bb38883ec3533394fdd61781
Your Name <you@example.com>
2024-12-03 14:46:50 +0100

    solution part 2

--- Day03/Day3.ts
+++ Day03/Day3.ts
@@ -1,7 +1,7 @@
 
 import { assert } from 'console';
 import * as fs from 'fs';
-import { get } from 'http';
+
 const readInputFile = (filePath: string): string[] => {
     const inputString = fs.readFileSync(filePath, 'utf-8');
 
@@ -14,7 +14,9 @@ const readInputFile = (filePath: string): string[] => {
 const regexPart1 = /mul\((\d{1,3}),(\d{1,3})\)/g;
 const regexPart2 = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't()\(\)/g;
 
-const getMatches = (line: string): string[] => line.match(new RegExp(regexPart1, 'g')) || [];
+const extractMatches = (input: string, regex: RegExp): string[] => {
+    return Array.from(input.matchAll(regex), match => match[0]);
+}
 
 const calculateSumOfMultiplications = (input: string): number => {
     let sum = 0;
@@ -33,17 +35,26 @@ const calculateSumOfMultiplications = (input: string): number => {
     return sum;
 };
 
-const extractValidMatches = (input: string): string => {
-    let result = '';
-    let match;
-    regexPart2.lastIndex = 0;
+const extractValidMatches = (input: string): string[] => {
+    const validMatches: string[] = [];
 
-    while ((match = regexPart2.exec(input)) !== null) {
-        result += match[0];
+    const matches = input.match(regexPart2) || [];
+    let currentPrefix: "do" | "dont" | null = null;
+
+    for (const match of matches) {
+        if (match === "do()") {
+            currentPrefix = "do";
+        } else if (match === "don't()") {
+            currentPrefix = "dont";
+        } else if (match.startsWith("mul")) {
+            if (currentPrefix !== "dont") {
+                validMatches.push(match);
+            }
+        }
     }
 
-    return result;
-};
+    return validMatches;
+}
 
 
 const main = () => {
@@ -59,8 +70,13 @@ const main = () => {
     console.log(sum2); */
     const simpleExample: string = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
     const result = extractValidMatches(simpleExample);
-    assert(result === "mul(2,4)don't()mul(5,5)mul(11,8)do()mul(8,5)", "Simple example failed");
-    console.log(result);
-}
+    const sum2 = calculateSumOfMultiplications(result.join(""));
+    assert(sum2 === 48, "Simple example failed");
+    console.log(sum2);
 
+    const input: string[] = readInputFile("InputDay3.txt");
+    const inputString = input.join("\n");
+    const sum3 = calculateSumOfMultiplications(extractValidMatches(inputString).join(""));
+    console.log(sum3);
+}
 main();
