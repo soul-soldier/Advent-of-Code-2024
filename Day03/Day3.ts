7abfbe3ec2a55c7ab12a33bb27f88dddcd05fbd6
Your Name <you@example.com>
2024-12-03 14:14:30 +0100

    added extractValidMatches

--- Day03/Day3.ts
+++ Day03/Day3.ts
@@ -12,7 +12,8 @@ const readInputFile = (filePath: string): string[] => {
 }
 
 const regexPart1 = /mul\((\d{1,3}),(\d{1,3})\)/g;
-const regexPart2 = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const regexPart2 = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't()\(\)/g;
+
 const getMatches = (line: string): string[] => line.match(new RegExp(regexPart1, 'g')) || [];
 
 const calculateSumOfMultiplications = (input: string): number => {
@@ -24,13 +25,26 @@ const calculateSumOfMultiplications = (input: string): number => {
     while ((match = regexPart1.exec(input)) !== null) {
         const num1 = parseInt(match[1], 10);
         const num2 = parseInt(match[2], 10);
-        console.log(num1, num2);
+        //console.log(num1, num2);
         sum += num1 * num2;
-        console.log(`Match found: ${match[0]} => ${num1} * ${num2} = ${num1 * num2}`);
+        //console.log(`Match found: ${match[0]} => ${num1} * ${num2} = ${num1 * num2}`);
     }
 
     return sum;
 };
+
+const extractValidMatches = (input: string): string => {
+    let result = '';
+    let match;
+    regexPart2.lastIndex = 0;
+
+    while ((match = regexPart2.exec(input)) !== null) {
+        result += match[0];
+    }
+
+    return result;
+};
+
 
 const main = () => {
     const inputSimple: string[] = readInputFile("InputDay3Simple.txt");
@@ -39,10 +53,14 @@ const main = () => {
     assert(sum1 === 161, "Simple example failed");
     console.log(sum1);
 
-    const input: string[] = readInputFile("InputDay3.txt");
+    /* const input: string[] = readInputFile("InputDay3.txt");
     const inputString = input.join("\n");
     const sum2 = calculateSumOfMultiplications(inputString);
-    console.log(sum2);
+    console.log(sum2); */
+    const simpleExample: string = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    const result = extractValidMatches(simpleExample);
+    assert(result === "mul(2,4)don't()mul(5,5)mul(11,8)do()mul(8,5)", "Simple example failed");
+    console.log(result);
 }
 
 main();
